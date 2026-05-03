97bc672ecebfd9cedcb6e60f67bc58642876165e
beealexander <abrandy@wustl.edu>
2026-05-03 18:35:47 -0500

    Update unicode.js

--- unicode.js
+++ unicode.js
@@ -86,20 +86,20 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = input.charCodeAt(0);
+let thirdCodePoint = input.charCodeAt(2);
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints = String.fromCharCode(72, 101, 108, 108);
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
+let swappedString = lastChar + middle + firstChar;
 
 // Log all results
 console.log({
-  firstCodePoint,
-  thirdCodePoint,
-  wordFromCodePoints,
-  swappedString,
+  firstCodePoint = 67
+  thirdCodePoint = 100
+  wordFromCodePoints = Hell
+  swappedString = hauncL
 });
